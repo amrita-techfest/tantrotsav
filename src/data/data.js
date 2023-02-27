4ecba95733998590069d8065fc00f460815d2997
cool-man-vk <rrvigneshkumar2002@gmail.com>
2023-02-27 09:55:24 +0530

    commit

---
+++ src/data/data.js
@@ -0,0 +1,18 @@
+export const events = [
+    {
+        dept:'CSE',
+        events:[
+            'Strigrays League - Gaming Jam',
+            'Battle Of The Ice - Gaming Tournment',
+            'Zh3r0-Capture the Flag',
+            'Platonic',
+            'Bidders Coding Camp',
+            'BotBattleBash B3',
+            'ML-XLR8',
+            'AI Escape Room',
+            'Colay',
+            'Dare To be Different (Ideathon)',
+            'Forensics Investigation Challenge'
+        ]
+    }
+];
