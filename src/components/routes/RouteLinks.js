285aa8685e1ad0dad836440b9bec0e362c11992a
RahulDiwakar <rahuldiwakar.r.s@gmail.com>
2023-03-05 23:33:47 +0530

    'gamepage'

--- src/components/routes/RouteLinks.js
+++ src/components/routes/RouteLinks.js
@@ -2,28 +2,28 @@ import { Route, Routes } from "react-router-dom";
 import EventsIndex from "../events/index/index.js";
 import Home from "../Home";
 import Footer from "../Footer";
-import Register from "../auth/register.js"
+import Register from "../auth/register.js";
+import Game from "../Game.js";
 // import EventsIndex from "../events/index/index.js";
 
-
 import Dashboard from "../user/dashboard.js";
-import RegistedEvents from "../user/eventcard.js"
-import Profile from "../user/profile.js"
+import RegistedEvents from "../user/eventcard.js";
+import Profile from "../user/profile.js";
+import ParticleBackground from "../ParticleBackground";
 
 import React from "react";
 import EventDetails from "../events/eventDetails/eventDetails.js";
 
-
-
 const RouteLinks = () => {
   return (
     <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/events" element={<EventsIndex />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/Footer" element={<Footer />} />
-        <Route path="/events/details" element={<EventDetails />} />
-
+      <Route path="/" element={<Home />} />
+      <Route path="/events" element={<EventsIndex />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/Footer" element={<Footer />} />
+      <Route path="/events/details" element={<EventDetails />} />
+      <Route path="/Game" element={<Game />} />
+      <Route path="/ParticleBackground" element={<ParticleBackground />} />
       <Route path="/Footer" element={<Footer />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/registedEvents" element={<RegistedEvents />} />
--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import Footer from "./components/Footer";
-
+import Navbar from "./components/Navbar";
+import Game from "./components/Game";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
+  <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
