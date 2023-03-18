752dfebae8ba20a8ace234af7d8a8dd5375ea21e
thekavikumar <kavikumarceo@gmail.com>
2023-03-18 10:47:29 +0530

    register updated

--- src/components/auth/register-form/personalInfo.js
+++ src/components/auth/register-form/personalInfo.js
@@ -3,6 +3,12 @@ import { connect } from "react-redux";
 import "./styles.css";
 import { addPersonalInfo } from "./redux/actions";
 import generateID from "../../../utils/GenrateID";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
+import { auth } from "../../../firebase";
 
 const PersonalInfo = ({
   addPersonalInfo,
@@ -12,17 +18,22 @@ const PersonalInfo = ({
 }) => {
   const [fullName, setFullName] = React.useState("");
   const [universityName, setUniversityName] = React.useState("");
+  const [isAmrita, setIsAmrtia] = React.useState("");
   const [userEmail, setUserEmail] = React.useState("");
   const [userWhatsAppNumber, setUserWhatsAppNumber] = React.useState("");
   const [Id, setId] = useState();
 
-  const onChange = event => {
+  const onChange = (event) => {
     const { name, value } = event.target;
     if (name === "fullName") {
       setFullName(value);
     } else if (name === "universityName") {
       setUniversityName(value);
     }
+  };
+
+  const handleChange = (event) => {
+    setIsAmrtia(event.target.value);
   };
 
   useEffect(() => {
@@ -37,6 +48,7 @@ const PersonalInfo = ({
     if (
       fullName !== "" &&
       universityName !== "" &&
+      isAmrita !== "" &&
       userEmail !== "" &&
       userWhatsAppNumber !== "" &&
       Id !== ""
@@ -45,6 +57,7 @@ const PersonalInfo = ({
       addPersonalInfo({
         fullName,
         universityName,
+        isAmrita,
         userEmail,
         userWhatsAppNumber,
         Id,
@@ -53,6 +66,7 @@ const PersonalInfo = ({
     addPersonalInfo,
     fullName,
     universityName,
+    isAmrita,
     userEmail,
     userWhatsAppNumber,
     Id,
@@ -62,6 +76,7 @@ const PersonalInfo = ({
     if (
       fullName !== "" &&
       universityName !== "" &&
+      isAmrita !== "" &&
       userEmail !== "" &&
       userWhatsAppNumber !== ""
     ) {
@@ -70,7 +85,7 @@ const PersonalInfo = ({
   };
 
   return (
-    <div className='parent-content'>
+    <div className="parent-content">
       <div
         style={{
           display: "flex",
@@ -93,86 +108,114 @@ const PersonalInfo = ({
           Please help us with some of your Personal Details
         </h2>
         <form>
-          <div className='mb-1'>
-            <label for='IDInput' className='form-label'>
+          <div className="mb-1">
+            <label for="IDInput" className="form-label">
               ID
             </label>
             <input
-              type='text'
-              className='form-control'
-              id='IDInput'
-              name='Id'
+              type="text"
+              className="form-control"
+              id="IDInput"
+              name="Id"
               disabled
               value={Id}
             />
           </div>
-          <div className='mb-1'>
-            <label for='fullNameInput' className='form-label'>
+          <div className="mb-1">
+            <label for="fullNameInput" className="form-label">
               Full Name
             </label>
             <input
-              type='text'
-              className='form-control'
-              id='fullNameInput'
-              name='fullName'
+              type="text"
+              className="form-control"
+              id="fullNameInput"
+              name="fullName"
               onChange={onChange}
               value={fullName}
             />
           </div>
-          <div className='mb-1'>
-            <label for='emailInput' className='form-label'>
+          <FormControl>
+            <FormLabel
+              id="demo-row-radio-buttons-group-label"
+              className="text-white mt-1"
+            >
+              Are you a student of Amrita Vishwa Vidyapeetham?
+            </FormLabel>
+            <RadioGroup
+              row
+              aria-labelledby="demo-row-radio-buttons-group-label"
+              name="row-radio-buttons-group"
+              className="text-white"
+              onChange={handleChange}
+            >
+              <FormControlLabel
+                value="Yes"
+                className="text-white"
+                control={<Radio className="text-white" />}
+                label="Yes"
+              />
+              <FormControlLabel
+                value="No"
+                className="text-white"
+                control={<Radio color="success" className="text-white" />}
+                label="No"
+              />
+            </RadioGroup>
+          </FormControl>
+          <div className="mb-1">
+            <label for="emailInput" className="form-label">
               Email address
             </label>
             <input
-              type='email'
-              className='form-control'
-              id='emailInput'
-              aria-describedby='emailInfo'
-              onChange={e => {
+              type="email"
+              className="form-control"
+              id="emailInput"
+              aria-describedby="emailInfo"
+              onChange={(e) => {
                 setUserEmail(e.target.value);
               }}
               value={userEmail}
             />
-            <div id='emailInfo' className='form-text'>
+            <div id="emailInfo" className="form-text">
               We'll never spam you and share your email with anyone else.
             </div>
           </div>
-          <div className='mb-1'>
-            <label for='phoneNumberInput' className='form-label'>
+          <div className="mb-1">
+            <label for="phoneNumberInput" className="form-label">
               WhatsApp Number
             </label>
             <input
-              type='tel'
-              className='form-control'
-              id='phoneNumberInput'
-              pattern='[0-9]{3} [0-9]{3} [0-9]{4}'
-              maxlength='10'
-              onChange={e => {
+              type="tel"
+              className="form-control"
+              id="phoneNumberInput"
+              pattern="[0-9]{3} [0-9]{3} [0-9]{4}"
+              maxlength="10"
+              onChange={(e) => {
                 setUserWhatsAppNumber(e.target.value);
               }}
               value={userWhatsAppNumber}
             />
           </div>
-          <div className='mb-1'>
-            <label for='collegeInput' className='form-label'>
+          <div className="mb-1">
+            <label for="collegeInput" className="form-label">
               College/University Name
             </label>
             <input
-              type='text'
-              className='form-control'
-              id='collegeInput'
-              name='universityName'
-              onChange={e => {
+              type="text"
+              className="form-control"
+              id="collegeInput"
+              name="universityName"
+              onChange={(e) => {
                 setUniversityName(e.target.value);
               }}
               value={universityName}
             />
           </div>
         </form>
-        <div className='buttons mt-4'>
+        <div className="buttons mt-4">
           <button
             onClick={goNext}
-            className=' border-2 border-[#0dff00] transition duration-500 hover:bg-[#0dff00] hover:font-bold text-[16px] hover:text-black p-2 rounded-[5px] w-[130px]'
+            className=" border-2 border-[#0dff00] transition duration-500 hover:bg-[#0dff00] hover:font-bold text-[16px] hover:text-black p-2 rounded-[5px] w-[130px]"
           >
             Go Next
           </button>
@@ -182,13 +225,13 @@ const PersonalInfo = ({
   );
 };
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   authData: state.eventLists.authData,
   personalInfo: state.eventLists.personalInfo,
 });
 
-const mapDispatchToProps = dispatch => ({
-  addPersonalInfo: personalInfo => dispatch(addPersonalInfo(personalInfo)),
+const mapDispatchToProps = (dispatch) => ({
+  addPersonalInfo: (personalInfo) => dispatch(addPersonalInfo(personalInfo)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(PersonalInfo);
--- src/components/auth/register-form/registrationFeePayment.js
+++ src/components/auth/register-form/registrationFeePayment.js
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { Navigate, redirect, useNavigate } from "react-router-dom";
 import { registerUserStart } from "./redux/actions";
 import "./styles.css";
+import Checkbox from "@mui/material/Checkbox";
+import { auth } from "../../../firebase";
 
 const RegistrationFeePayment = ({
   authData,
@@ -12,6 +14,9 @@ const RegistrationFeePayment = ({
   personalInfo,
 }) => {
   const [transactionID, setTransactionID] = useState("");
+  const [termsChecked, setTermsChecked] = React.useState(false);
+  const [policyChecked, setPolicyChecked] = React.useState(false);
+
   // const [paymentProof, setPaymentProof] = useState(null);
   const navigate = useNavigate();
 
@@ -19,19 +24,38 @@ const RegistrationFeePayment = ({
     window.open("https://payment.tantrotsav.co.in/", "_blank");
   };
 
+  const handleChange = (event) => {
+    setPolicyChecked(!policyChecked);
+  };
+
+  const handleChange1 = (event) => {
+    setTermsChecked(!termsChecked);
+  };
+
+  const user = auth.currentUser;
+
+  useEffect(() => {
+    console.log(termsChecked, policyChecked);
+  }, [termsChecked, policyChecked]);
+
   const handleRegistration = () => {
     const userData = {
       fullName: personalInfo.fullName,
-      email: authData.userEmail,
-      whatsappNumber: authData.userWhatsAppNumber,
+      email: user.email,
+      whatsappNumber: personalInfo.userWhatsAppNumber,
+      isAmrita: personalInfo.isAmrita,
       universityName: personalInfo.universityName,
       individualEvents,
       teamEvents,
       transactionID,
     };
 
-    registerUser(userData);
-    return navigate("/");
+    if (termsChecked && policyChecked) {
+      registerUser(userData);
+      return navigate("/");
+    } else {
+      alert("Please agree to the terms and conditions and privacy policy");
+    }
   };
 
   const individualEventsTotalFees = individualEvents?.reduce(
@@ -49,29 +73,62 @@ const RegistrationFeePayment = ({
   console.log(totalFees);
 
   return (
-    <div className='parent-content'>
-      <div className='registrationFees-banner'>
-        <h2 className='mb-5'>
+    <div className="parent-content">
+      <div className="registrationFees-banner">
+        <h2 className="mb-5">
           Continue by paying the registration fees: Rs. {totalFees}
         </h2>
-        <button className='paymentButton mt-5 mb-5' onClick={handleClick}>
+        <button className="paymentButton mt-5 mb-5" onClick={handleClick}>
           Go to Payment Portal
         </button>
-        <div class='mt-5 mb-3'>
-          <label for='transactionIdField' className='form-label'>
-            Transaction Id
+        <div class="mt-5 mb-3">
+          <label for="transactionIdField" className="form-label">
+            Tracking Id
           </label>
           <input
-            type='text'
-            class='form-control'
-            id='transactionIdField'
+            type="text"
+            class="form-control"
+            id="transactionIdField"
             value={transactionID}
-            onChange={e => setTransactionID(e.target.value)}
+            onChange={(e) => setTransactionID(e.target.value)}
           />
         </div>
-
-        <div className='mb-3 mt-2'>
-          <button className='registerButton' onClick={handleRegistration}>
+        <div className="">
+          <div className="flex justify-between items-center">
+            <Checkbox
+              onChange={handleChange1}
+              inputProps={{ "aria-label": "controlled" }}
+            />
+            <h1>
+              I agree to the{" "}
+              <a
+                className=" text-[#0004ff]"
+                href="https://docs.google.com/document/d/1YsFydt4t5BwaJlHd1p_B1JQYN3v8wWAxwAAu1mCtyx8/edit?usp=sharing"
+                target="_blank"
+              >
+                terms & conditions
+              </a>
+            </h1>
+          </div>
+          <div className="flex items-center">
+            <Checkbox
+              onChange={handleChange}
+              inputProps={{ "aria-label": "controlled" }}
+            />
+            <h1>
+              I have read the{" "}
+              <a
+                href="https://docs.google.com/document/d/1ECe1lcXE_xdhEWRYO3YMsBayxLYA6KI2ZmR2DzbNNX8/edit?usp=sharing"
+                target="_blank"
+                className=" text-[#0004ff]"
+              >
+                privacy policy
+              </a>
+            </h1>
+          </div>
+        </div>
+        <div className="mb-3 mt-2">
+          <button className="registerButton" onClick={handleRegistration}>
             Register
           </button>
         </div>
@@ -80,15 +137,15 @@ const RegistrationFeePayment = ({
   );
 };
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   authData: state.eventLists.authData,
   personalInfo: state.eventLists.personalInfo,
   individualEvents: state.eventLists.individualEvents,
   teamEvents: state.eventLists.teamEvents,
 });
 
-const mapDispatchToProps = dispatch => ({
-  registerUser: registrationDetails =>
+const mapDispatchToProps = (dispatch) => ({
+  registerUser: (registrationDetails) =>
     dispatch(registerUserStart(registrationDetails)),
 });
 
