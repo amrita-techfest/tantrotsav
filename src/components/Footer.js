3b38c5a2cf6992ad3390647f1d70a111a9611430
thekavikumar <kavikumarceo@gmail.com>
2023-03-20 19:41:17 +0530

    final updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,57 +3,65 @@ import "./Footer.css";
 
 const Footer = () => {
   return (
-    <footer class='footer-distributed' id='contact'>
-      <div class='footer-left'>
+    <footer class="footer-distributed" id="contact">
+      <div class="footer-left">
         {/* <h3>
           Company<span>logo</span>
         </h3> */}
         <img
-          src='https://res.cloudinary.com/thydreams/image/upload/v1678515578/tantrotsav/hmsygeddwaoyc66sqgui.png'
-          alt='logo_footer'
-          className='w-[300px] h-[200px] object-contain'
+          src="https://res.cloudinary.com/thydreams/image/upload/v1678515578/tantrotsav/hmsygeddwaoyc66sqgui.png"
+          alt="logo_footer"
+          className="w-[300px] h-[200px] object-contain"
         />
       </div>
 
-      <div class='footer-center'>
+      <div class="footer-center">
         <div>
-          <a href='https://goo.gl/maps/zjZpcok8QE9cNfeH7'>
-            <i class='fa fa-map'></i>
+          <a href="https://goo.gl/maps/zjZpcok8QE9cNfeH7">
+            <i class="fa fa-map"></i>
           </a>
           <p>
-            <a href='https://goo.gl/maps/zjZpcok8QE9cNfeH7'>Google Maps</a>
+            <a href="https://goo.gl/maps/zjZpcok8QE9cNfeH7">Google Maps</a>
           </p>
         </div>
         <div>
-          <a href='tel:04435533222'>
-            <i class='fa fa-phone'></i>
+          <a href="tel:04435533222">
+            <i class="fa fa-phone"></i>
           </a>
           <p>
-            <a href='tel:04435533222'>+91 44 35533222</a>
+            <a href="tel:04435533222">+91 44 35533222</a>
+          </p>
+        </div>
+        <div>
+          <a href="tel:04435533222">
+            <i class="fa fa-phone"></i>
+          </a>
+          <p>
+            <a href="tel:9025313327">+91 9025313327</a>
           </p>
         </div>
 
         <div>
-          <a href='mailto:tantrotsav@ch.amrita.edu'>
-            <i class='fa fa-envelope'></i>
+          <a href="mailto:tantrotsav@ch.amrita.edu">
+            <i class="fa fa-envelope"></i>
           </a>
           <p>
-            <a href='mailto:tantrotsav@ch.amrita.edu'>
+            <a href="mailto:tantrotsav@ch.amrita.edu">
               tantrotsav@ch.amrita.edu
             </a>
           </p>
         </div>
       </div>
 
-      <div class='footer-right'>
-        <p class='footer-company-about'>
+      <div class="footer-right">
+        <p class="footer-company-about">
           <span>College Address</span>
           Amrita Vishwa Vidyapeetham Chennai Campus 337/1A, Vengal Village,
           Thiruvallur Taluk & District – 601 103. Tamil Nadu, India
         </p>
-        <div class='footer-icons'>
+        <div class="footer-icons">
           <button
-            className='border-2 border-[#0dff00] transition duration-500 hover:bg-[#0dff00] hover:font-bold text-[16px] hover:text-black p-2 rounded-[5px] w-[130px]'
+            className="border-2 border-[#0dff00] transition duration-500 hover:bg-[#0dff00] hover:font-bold text-[16px] hover:text-black p-2 rounded-[5px] w-[130px]"
             onClick={() => {
               window.open(
                 "https://amritacampuschennai-my.sharepoint.com/:b:/g/personal/ch_en_u4eee19008_ch_students_amrita_edu/ETTxqZOCa9dNnHsImU8sbN4BLrQ0F0YVV4F7zE8PpURCdw?download=1"
@@ -65,9 +73,9 @@ const Footer = () => {
         </div>
       </div>
       {/* <h1 className='text-center'>Made with 💖 by Team Tantrotsav</h1> */}
-      <h1 className='text-center mt-5'>
+      <h1 className="text-center mt-5">
         2023@copyright{" "}
-        <span className=' text-[#3f0]'>All rights are reserved</span>{" "}
+        <span className=" text-[#3f0]">All rights are reserved</span>{" "}
       </h1>
     </footer>
     // <footer id='contact'>
